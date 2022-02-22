b3c6ea519506cd82482bdb168d1d94b457df83af
dongdi <dongdi@p1staff.com>
2022-02-22 19:20:44 +0800

    feat: init

---
+++ 设计模式/server1.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let todos = [{id:1, text: 'a', complete: false},{id:2, text: 'a2', complete: false},{id:3, text: 'a3', complete: false},{id:4, text: 'a4', complete: false}];
+      
+app.get('/todos', function(req, res) {
+  res.sendFile(path.join(__dirname, 'todo.html'));
+})
+
+app.get('/getTodos', function(req, res) {
+  res.json(todos);
+})
+
+app.get('/toggle/:id', function(req, res) {
+  let id = req.params.id;
+  todos = todos.map((_todo) => {
+    if (_todo.id == id) {
+      _todo.complete = !_todo.complete
+    } 
+    return _todo;
+  })
+  res.json(todos);
+})
+
+app.listen(7001, function() {
+  console.log('listen 7001');
+})
